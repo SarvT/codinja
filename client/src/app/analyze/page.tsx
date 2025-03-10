dd9d5a5268d2f59306715e1254ccc2d104940762
SarvT <91484197+SarvT@users.noreply.github.com>
2025-03-10 14:12:14 +0530

    useEffect utilization

--- client/src/app/analyze/page.tsx
+++ client/src/app/analyze/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import {
